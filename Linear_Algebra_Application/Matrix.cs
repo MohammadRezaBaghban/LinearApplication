3cf72cd7b476b417fbe6f556549960a3ac8334d1
Mrbh <Mrbhmr@gmail.com>
2021-05-08 18:35:46 +0200

    Add Matrxi Class with Dot_Product() Vector_Length() Angle_Two_Vector() and Cosine_Reverse() methods

---
+++ Linear_Algebra_Application/Matrix.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Linear_Algebra_Application
+{
+    public static class Matrix
+    {
+
+        public static int Dot_Product(int[] array1, int[] array2)
+        {
+            if (array1.Length != array2.Length || array1.Length==0 || array2.Length==0)
+                throw new Exception("Size of array is not same");
+
+            var sum = 0;
+            for (int i = 0; i < array1.Length; i++) { sum += array1[i] * array2[i]; }
+            return sum;
+        }
+
+        public static double Vector_Length(int[] array)
+        {
+            if(array.Length<1)
+                throw new Exception("Array size should be more than 1");
+
+            var sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += (int)Math.Pow(array[i], 2);
+            }
+            return Math.Sqrt(sum);
+        }
+
+        public static double Angle_Two_Vector(int[] array1, int[] array2)
+        {
+            var numerator = Dot_Product(array1, array2);
+            var denominator = Vector_Length(array1) * Vector_Length(array2);
+            return numerator/ denominator;
+        }
+
+        public static double Cosine_Reverse(double input)
+        {
+            var angle = Math.Acos(input) * 180 / Math.PI;
+            return Math.Round(angle, 2);
+        }
+
+    }
+}
