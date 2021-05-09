9fe0a4e54a3d488f7b7b3586278128708c3b5629
Mrbh <Mrbhmr@gmail.com>
2021-05-09 16:40:14 +0200

    Add some GUI element to work with vectors and adding vectors

--- Linear_Algebra_Application/Form1.Designer.cs
+++ Linear_Algebra_Application/Form1.Designer.cs
@@ -29,13 +29,124 @@ namespace Linear_Algebra_Application
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.vectorList = new System.Windows.Forms.ListBox();
+            this.btn_Vector1 = new System.Windows.Forms.Button();
+            this.lb_array1 = new System.Windows.Forms.Label();
+            this.lb_array2 = new System.Windows.Forms.Label();
+            this.btn_Vector2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(146, 33);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(528, 45);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Vector:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(705, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // vectorList
+            // 
+            this.vectorList.FormattingEnabled = true;
+            this.vectorList.ItemHeight = 38;
+            this.vectorList.Location = new System.Drawing.Point(146, 101);
+            this.vectorList.Name = "vectorList";
+            this.vectorList.Size = new System.Drawing.Size(332, 156);
+            this.vectorList.TabIndex = 3;
+            this.vectorList.SelectedIndexChanged += new System.EventHandler(this.vectorList_SelectedIndexChanged);
+            // 
+            // btn_Vector1
+            // 
+            this.btn_Vector1.Location = new System.Drawing.Point(484, 103);
+            this.btn_Vector1.Name = "btn_Vector1";
+            this.btn_Vector1.Size = new System.Drawing.Size(82, 66);
+            this.btn_Vector1.TabIndex = 4;
+            this.btn_Vector1.Text = "1";
+            this.btn_Vector1.UseVisualStyleBackColor = true;
+            this.btn_Vector1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lb_array1
+            // 
+            this.lb_array1.AutoSize = true;
+            this.lb_array1.Location = new System.Drawing.Point(603, 103);
+            this.lb_array1.Name = "lb_array1";
+            this.lb_array1.Size = new System.Drawing.Size(106, 38);
+            this.lb_array1.TabIndex = 5;
+            this.lb_array1.Text = "Array 1";
+            // 
+            // lb_array2
+            // 
+            this.lb_array2.AutoSize = true;
+            this.lb_array2.Location = new System.Drawing.Point(603, 170);
+            this.lb_array2.Name = "lb_array2";
+            this.lb_array2.Size = new System.Drawing.Size(106, 38);
+            this.lb_array2.TabIndex = 6;
+            this.lb_array2.Text = "Array 2";
+            // 
+            // btn_Vector2
+            // 
+            this.btn_Vector2.Location = new System.Drawing.Point(484, 191);
+            this.btn_Vector2.Name = "btn_Vector2";
+            this.btn_Vector2.Size = new System.Drawing.Size(82, 66);
+            this.btn_Vector2.TabIndex = 7;
+            this.btn_Vector2.Text = "2";
+            this.btn_Vector2.UseVisualStyleBackColor = true;
+            this.btn_Vector2.Click += new System.EventHandler(this.btn_Vector2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 38F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1218, 424);
+            this.Controls.Add(this.btn_Vector2);
+            this.Controls.Add(this.lb_array2);
+            this.Controls.Add(this.lb_array1);
+            this.Controls.Add(this.btn_Vector1);
+            this.Controls.Add(this.vectorList);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox vectorList;
+        private System.Windows.Forms.Button btn_Vector1;
+        private System.Windows.Forms.Label lb_array1;
+        private System.Windows.Forms.Label lb_array2;
+        private System.Windows.Forms.Button btn_Vector2;
     }
 }
 
--- Linear_Algebra_Application/Form1.cs
+++ Linear_Algebra_Application/Form1.cs
@@ -1,20 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.String;
 
 namespace Linear_Algebra_Application
 {
     public partial class Form1 : Form
     {
+        List<Vector> memberRating;
         public Form1()
         {
             InitializeComponent();
+            memberRating = new List<Vector>()
+            {
+                new Vector(new[] { 2, 1, 2, -2, -1, 0, 1, 0, -1, 1 }),
+                new Vector(new[] { 2, -2, 1, 2, 1, -1, 0, 2, -1, 1 }),
+                new Vector(new[] { 1, 1, -2, 0, -2, -2, 2, 2, 1, 2 }),
+                new Vector(new[] { 1, 0, -2, 1, 2, 2, 1, 0, -1, -2 }),
+                new Vector(new[] { 1, -1, 1, 1, 2, 0, 2, 1, 0, 1 }),
+                new Vector(new[] { 2, 1, -2, 1, 0, 0, 0, 0, 0, 0 })
+            };
+
+            memberRating.ForEach(x=> vectorList.Items.Add(x));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<int> array = new List<int>();
+            var input = textBox1.Text.Replace(" ", "").Split(',').ToList();
+            input.ForEach(x => array.Add(Convert.ToInt32(x)));
+            var vector = new Vector(array.ToArray());
+            vectorList.Items.Add(vector);
+        }
+
+        private void vectorList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (vectorList.SelectedItem != null)
+            {
+                var selectedItem = vectorList.SelectedItem as Vector;
+                lb_array1.Text = $"Array 1: {selectedItem.ToString()}";
+                lb_array1.Tag = selectedItem;
+            }
+        }
+
+        private void btn_Vector2_Click(object sender, EventArgs e)
+        {
+            if (vectorList.SelectedItem != null)
+            {
+                var selectedItem = vectorList.SelectedItem as Vector;
+                lb_array2.Text = $"Array 2: {selectedItem.ToString()}";
+                lb_array2.Tag = selectedItem;
+            }
+        }
+    }
+
+    public class Vector
+    {
+        private int[] value;
+
+        public Vector(int[] array) => this.value = array;
+
+        public override string ToString()
+        {
+            return "[" + Join(",", value) + "]";
         }
     }
 }
